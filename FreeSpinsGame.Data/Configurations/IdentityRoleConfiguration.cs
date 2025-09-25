70bc40d38344439c28300379e9432fecb8adca79
nikolaiandrov <nikolaiandrov@abv.bg>
2025-09-25 23:02:26 +0300

    config for seeding IdentityRole added

---
+++ FreeSpinsGame.Data/Configurations/IdentityRoleConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static FreeSpinsGame.Common.GeneralApplicationConstants;
+
+namespace FreeSpinsGame.Data.Configurations
+{
+    internal class IdentityRoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(this.GenerateRoles());
+        }
+
+        private List<IdentityRole> GenerateRoles()
+        {
+            List<IdentityRole> roles = new List<IdentityRole>();
+
+            IdentityRole role;
+
+            role = new IdentityRole
+            {
+                Id = "651d64a8-7378-4ee9-8916-776f2bb45d01",
+                Name = AdminRoleName,
+                NormalizedName = AdminRoleName.ToUpper()
+            };
+            roles.Add(role);
+
+            role = new IdentityRole
+            {
+                Id = "651d64a8-7378-4ee9-8916-776f2nm45d01",
+                Name = UserRoleName,
+                NormalizedName = UserRoleName.ToUpper()
+            };
+            roles.Add(role);
+
+            return roles;
+        }
+    }
+}
