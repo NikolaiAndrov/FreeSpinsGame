85c161a02647424de04d7e68d37e05aed294269e
nikolaiandrov <nikolaiandrov@abv.bg>
2025-09-25 23:01:15 +0300

    dto project added and clas for registerplayerdto

---
+++ FreeSpinsGame.WebApi.DtoModels/RegisterPlayerDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static FreeSpinsGame.Common.EntityValidationConstants.RegisterPlayerDtoValidation;
+
+namespace FreeSpinsGame.WebApi.DtoModels
+{
+    public class RegisterPlayerDto
+    {
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength)]
+        public string Password { get; set; } = null!;
+    }
+}
