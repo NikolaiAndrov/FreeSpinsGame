15229ecc66e61789d520fd2c34b71a956b00a092
nikolaiandrov <nikolaiandrov@abv.bg>
2025-09-25 23:03:52 +0300

    Account Controller added

---
+++ FreeSpinsGame.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using FreeSpinsGame.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeSpinsGame.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<Player> userManager;
+        private readonly SignInManager<Player> signInManager;
+
+        public AccountController(UserManager<Player> userManager, SignInManager<Player> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+    }
+}
