c78786d5da37c5b64186f9fb551c6b56bb66fe75
nikolaiandrov <nikolaiandrov@abv.bg>
2025-09-26 23:49:57 +0300

    database migration applied for the concurrency stamp

--- FreeSpinsGame.Data/Migrations/20250926204906_concurrencystamp.Designer.cs
+++ FreeSpinsGame.Data/Migrations/20250926204906_concurrencystamp.Designer.cs
@@ -4,6 +4,7 @@ using FreeSpinsGame.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FreeSpinsGame.Data.Migrations
 {
     [DbContext(typeof(FreeSpinsGameDbContext))]
-    partial class FreeSpinsGameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250926204906_concurrencystamp")]
+    partial class concurrencystamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
