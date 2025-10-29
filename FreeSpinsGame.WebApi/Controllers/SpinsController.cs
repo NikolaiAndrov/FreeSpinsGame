7f817b79eddd13fe8a768d3e5baeab79509ad308
nikolaiandrov <nikolaiandrov@abv.bg>
2025-10-29 09:43:09 +0200

    SpinsController - updated for global ex handler, next think about dry

--- FreeSpinsGame.WebApi/Controllers/SpinsController.cs
+++ FreeSpinsGame.WebApi/Controllers/SpinsController.cs
@@ -30,104 +30,42 @@ namespace FreeSpinsGame.WebApi.Controllers
         [HttpPost("campaigns/{campaignId}/players/{playerId}/spin")]
         public async Task<IActionResult> Spin(Guid campaignId, string playerId)
         {
-            try
+            bool isPlayerExisting = await this.playerService.IsPlayerExistingByIdAsync(playerId);
+            bool isCampaignExisting = await this.campaignService.IsCampaignExistingByIdAsync(campaignId);
+            bool isSubscribed = await this.playerService.IsPlayerSubscribedToCampaignAsync(playerId, campaignId);
+
+            if (!isPlayerExisting || !isCampaignExisting || !isSubscribed)
             {
-                await this.ValidatePlayerAsync(playerId);
-                await this.ValidateCampaignAsync(campaignId);
-                await this.ValidatePlayerSubscriptionAsync(playerId, campaignId);
-                int remainingSpinCount = await this.spinService.SpinAsync(campaignId, playerId, DateTimeOffset.UtcNow);
-
-                if (remainingSpinCount < 0)
-                {
-                    return this.StatusCode(StatusCodes.Status403Forbidden, AllSpinsExhausted);
-                }
-
-                this.logger.LogInformation(SuccessfulSpin);
-
-                return this.Ok($"{RemainingSpinsCount} {remainingSpinCount}");
-            }
-            catch (ArgumentException ex)
-            {
-                this.logger.LogError(ex, ex.Message);
                 return this.BadRequest();
             }
-            catch (InvalidOperationException ex)
-            {
-                this.logger.LogError(ex, ex.Message);
-                return this.BadRequest();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                this.logger.LogError(ex, ConcurrencyConflict);
 
-                return this.StatusCode(StatusCodes.Status409Conflict, SpinConflict);
-            }
-            catch (Exception ex)
+            int remainingSpinCount = await this.spinService.SpinAsync(campaignId, playerId, DateTimeOffset.UtcNow);
+
+            if (remainingSpinCount < 0)
             {
-                this.logger.LogError(ex, UnexpectedErrorMessage);
-                return this.StatusCode(StatusCodes.Status500InternalServerError, UnexpectedErrorMessage);
+                return this.StatusCode(StatusCodes.Status403Forbidden, AllSpinsExhausted);
             }
+
+            this.logger.LogInformation(SuccessfulSpin);
+
+            return this.Ok($"{RemainingSpinsCount} {remainingSpinCount}");
         }
 
         [HttpGet("campaigns/{campaignId}/players/{playerId}/status")]
         public async Task<IActionResult> Status(Guid campaignId, string playerId)
         {
-            try
-            {
-                await this.ValidatePlayerAsync(playerId);
-                await this.ValidateCampaignAsync(campaignId);
-                await this.ValidatePlayerSubscriptionAsync(playerId, campaignId);
-
-                SpinStatusDto spinStatusDto = await this.spinService.GetSpinStatusAsync(campaignId, playerId, DateTimeOffset.UtcNow);
-                this.logger.LogInformation(StatusProvidedSuccessfully);
-
-                return this.Ok(spinStatusDto);
-            }
-            catch (ArgumentException ex) 
-            {
-                this.logger.LogError(ex, ex.Message);
-                return this.BadRequest();
-            }
-            catch (InvalidOperationException ex)
-            {
-                this.logger.LogError(ex, ex.Message);
-                return this.BadRequest();
-            }
-            catch (Exception ex)
-            {
-                this.logger.LogError(ex, UnexpectedErrorMessage);
-                return this.StatusCode(StatusCodes.Status500InternalServerError, UnexpectedErrorMessage);
-            }
-        }
-
-        private async Task ValidatePlayerAsync(string playerId)
-        {
             bool isPlayerExisting = await this.playerService.IsPlayerExistingByIdAsync(playerId);
-
-            if (!isPlayerExisting)
-            {
-                throw new ArgumentException(PlayerNotFound);
-            }
-        }
-
-        private async Task ValidateCampaignAsync(Guid campaignId)
-        {
             bool isCampaignExisting = await this.campaignService.IsCampaignExistingByIdAsync(campaignId);
-
-            if (!isCampaignExisting)
-            {
-                throw new ArgumentException(CampaignNotFound);
-            }
-        }
-
-        private async Task ValidatePlayerSubscriptionAsync(string playerId, Guid campaignId)
-        {
             bool isSubscribed = await this.playerService.IsPlayerSubscribedToCampaignAsync(playerId, campaignId);
 
-            if (!isSubscribed)
+            if (!isPlayerExisting || !isCampaignExisting || !isSubscribed)
             {
-                throw new InvalidOperationException(PlayerNotSubscribed);
+                return this.BadRequest();
             }
+
+            SpinStatusDto spinStatusDto = await this.spinService.GetSpinStatusAsync(campaignId, playerId, DateTimeOffset.UtcNow);
+            this.logger.LogInformation(StatusProvidedSuccessfully);
+
+            return this.Ok(spinStatusDto);
         }
     }
-}
